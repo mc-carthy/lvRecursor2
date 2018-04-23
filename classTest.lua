466be01c9dfbcd2888c00d1258556d267c279694
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-04-23 21:27:53 +0100

    Fix class module so classes can derive from other classes

---
+++ classTest.lua
@@ -0,0 +1,25 @@
+local Class = require('src.utils.class')
+
+local Animal = Class:derive('Animal')
+
+function Animal:soundOff()
+    print('uh?')
+end
+
+local a = Animal()
+a:soundOff()
+print(a:getType())
+
+local Cat = Animal:derive('Cat')
+
+function Cat:soundOff()
+    print('Meow!')
+end
+
+local c = Cat()
+c:soundOff()
+print(c:getType())
+
+local Minx = Cat:derive('Minx')
+local m = Minx()
+print(m:getType(), m.super:getType(), m.super.super:getType())
