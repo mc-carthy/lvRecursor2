08a632dd29bee8c0c02bd86b85cf7060b38d8c93
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-04-24 08:40:38 +0100

    Implement sprite class

--- main.lua
+++ main.lua
@@ -1,31 +1,29 @@
 -- require('src.utils.debug')
 local Animation = require('src.utils.animation')
+local Sprite = require('src.utils.sprite')
 
 local heroAtlas
-local heroSprite
-
-local fps = 15
-local animTime = 1 / fps
-local frame = 1
-local numFrames = 6
-local xOffset
-local cellWidth = 16
-
-local a = Animation(16, 32, 16, 16, 6, 6, 10)
+local spr
+local walkAnimation
+local swimAnimation
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     heroAtlas = love.graphics.newImage('src/assets/img/hero.png')
-    heroSprite = love.graphics.newQuad(16, 32, 16, 16, heroAtlas:getDimensions())
+    spr = Sprite(heroAtlas, 100, 100, 16, 16, 10, 10)
+    walkAnimation = Animation(16, 32, 16, 16, 6, 6, 10)
+    swimAnimation = Animation(16, 64, 16, 16, 6, 6, 10)
+    spr:addAnimation('walk', walkAnimation)
+    spr:addAnimation('swim', swimAnimation)
+    spr:animate('swim')
 end
 
 function love.update(dt)
-    a:update(dt, heroSprite)
+    spr:update(dt)
 end
 
 function love.draw()
-    -- love.graphics.draw(heroAtlas, 32, 32, 0, 4, 4)
-    love.graphics.draw(heroAtlas, heroSprite, 32, 32, 0, 10, 10)
+    spr:draw()
 end
 
 function love.keypressed(key)
---
+++ src/utils/sprite.lua
@@ -0,0 +1,40 @@
+local Class = require("src.utils.class")
+local Animation = require('src.utils.animation')
+local Vector2 = require("src.utils.vector2")
+
+local Sprite = Class:derive("Sprite")
+
+function Sprite:new(atlas, x, y, w, h, sx, sy, rot)
+    self.atlas = atlas
+    self.pos = Vector2(x, y)
+    self.size = Vector2(w, h)
+    self.scale = Vector2(sx or 1, sy or 1)
+    self.rot = rot or 0
+    self.animations = {}
+    self.currentAnimation = nil
+    self.quad = love.graphics.newQuad(0, 0, w, w, atlas:getDimensions())
+end
+
+function Sprite:addAnimation(name, anim)
+    self.animations[name] = anim
+end
+
+function Sprite:animate(animName)
+    if self.currentAnimation ~= animName and self.animations[animName] ~= nil then
+        self.animations[animName]:reset(self.quad)
+        self.animations[animName]:set(self.quad)
+        self.currentAnimation = animName
+    end
+end
+
+function Sprite:update(dt)
+    if self.animations[self.currentAnimation] ~= nil then
+        self.animations[self.currentAnimation]:update(dt, self.quad)
+    end
+end
+
+function Sprite:draw()
+    love.graphics.draw(self.atlas, self.quad, self.pos.x, self.pos.y, self.rot, self.scale.x, self.scale.y, self.size.x / 2, self.size.y / 2)
+end
+
+return Sprite
