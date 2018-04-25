7a0d473ed37c4842142b94642f1281144d3fe85f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-04-25 08:45:50 +0100

    Allow table of frame indices to be passed into an animation, as opposed to just a sequence

--- src/utils/animation.lua
+++ src/utils/animation.lua
@@ -2,9 +2,16 @@ local Class = require("src.utils.class")
 local Vector2 = require("src.utils.vector2")
 local Animation = Class:derive("Animation")
 
-function Animation:new(xOffset, yOffset, w, h, numFrames, columnSize, fps, loop)
+function Animation:new(xOffset, yOffset, w, h, frames, columnSize, fps, loop)
     self.fps = fps
-    self.numFrames = numFrames
+    if type(frames) == 'table' then
+        self.frames = frames
+    else
+        self.frames = {}
+        for i = 1, frames do
+            self.frames[i] = i
+        end
+    end
     self.columnSize = columnSize
     self.startOffset = Vector2(xOffset, yOffset)
     self.offset = Vector2()
@@ -14,24 +21,24 @@ function Animation:new(xOffset, yOffset, w, h, numFrames, columnSize, fps, loop)
 end
 
 function Animation:update(dt, quad)
-    if self.numFrames <= 1 then
+    if #self.frames <= 1 then
         return
     elseif self.timer > 0 then
         self.timer = self.timer - dt
         if self.timer <= 0 then
             self.timer = 1 / self.fps
-            self.frame = self.frame + 1
-            if self.frame > self.numFrames then
+            self.frameIndex = self.frameIndex + 1
+            if self.frameIndex > #self.frames then
                 if self.loop then
-                    self.frame = 1 
+                    self.frameIndex = 1 
                 else
-                    self.frame = self.numFrames
+                    self.frameIndex = #self.frames
                     self.timer = 0
                     self.done = true
                 end
             end
-            self.offset.x = self.startOffset.x + (self.size.x * ((self.frame - 1) % self.columnSize))
-            self.offset.y = self.startOffset.y + (self.size.y * math.floor((self.frame - 1) / self.columnSize))
+            self.offset.x = self.startOffset.x + (self.size.x * ((self.frames[self.frameIndex] - 1) % self.columnSize))
+            self.offset.y = self.startOffset.y + (self.size.y * math.floor((self.frames[self.frameIndex] - 1) / self.columnSize))
             self:set(quad)
         end
     end
@@ -43,7 +50,7 @@ end
 
 function Animation:reset()
     self.timer = 1 / self.fps
-    self.frame = 1
+    self.frameIndex = 1
     self.done = false
     self.offset.x = self.startOffset.x
     self.offset.y = self.startOffset.y
