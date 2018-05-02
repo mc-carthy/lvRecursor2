4c7e3e8c353ed0dfa2786ffd65c301f2c2ff7f0d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-05-02 21:45:53 +0100

    Create entity manager with drawing sorting layer enabled

---
+++ src/utils/entityManager.lua
@@ -0,0 +1,80 @@
+local Class = require('src.utils.class')
+
+local EntityManager = Class:derive('EntityManager')
+
+local function contains(tbl, item)
+    for k, v in pairs(tbl) do
+        return true
+    end
+    return false
+end
+
+local function compareLayers(e1, e2)
+    return e1.layer < e2.layer
+end
+
+function EntityManager:new()
+    self.entities = {}
+end
+
+function EM:onEnter()
+    for i = 1, #self.entities do
+        local e = self.entities[i]
+        if e.onEnter then e:onEnter() end
+    end
+end
+
+function EM:onExit()
+    for i = 1, #self.entities do
+        local e = self.entities[i]
+        if e.onExit then e:onExit() end
+    end
+end
+
+function EntityManager:add(entity)
+    if contains(self.entities, entity) then return end
+    
+    entity.layer = entity.layer or 1
+    entity.started = entity.started or false
+        
+    self.entities[#self.entities + 1] = entity
+    
+    table.sort(self.entities, compareLayers)
+end
+
+function EntityManager:remove(entity)
+
+end
+
+function EntityManager:update(dt)
+    for i = #self.entities, 1, -1 do
+        local e = self.entities[i]
+
+        if e.remove then
+            e.remove = false
+            if e.onRemove then
+                e.onRemove()
+            end
+            table.remove(self.entities, i)
+        end
+
+        if not e.started then
+            e.started = true
+            if e.onStart then
+                e.onStart()
+            end
+        else
+            e:update(dt)
+        end
+
+
+    end
+end
+
+function EntityManager:draw()
+    for i = 1, #self.entities do
+        self.entities[i]:draw()
+    end
+end
+
+return EntityManager
