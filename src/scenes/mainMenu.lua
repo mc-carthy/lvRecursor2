285b96401eadcb16484758ef89eaa4dd64b0a5d2
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-06-03 22:01:32 +0100

    Add vertical sliders

--- src/scenes/mainMenu.lua
+++ src/scenes/mainMenu.lua
@@ -23,7 +23,8 @@ function MainMenu:new(sceneManager)
     local startButton = Button(w / 2, h / 2 - 30, 100, 40, 'Start')
     local quitButton = Button(w / 2, h / 2 + 30, 100, 40, 'Quit')
     local menuText = Label(w / 2, 30, love.graphics.getWidth(), 40, 'Main Menu', { 255, 255, 255, 255})
-    local slider = Slider(love.graphics.getWidth() / 2 - 100, love.graphics.getHeight() - 25, 200, 10, 'TestSlider')
+    local horizontalSlider = Slider(love.graphics.getWidth() / 2, love.graphics.getHeight() - 25, 200, 10, 'HorizontalSlider')
+    local verticalSlider = Slider(20, love.graphics.getHeight() / 2, 10, 200, 'VerticalSlider', true)
     
     self.textField = TextField(w / 2, h / 2 + 90, 150, 40, 'Text Field', { 195, 195, 195, 255}, 'left')
     startButton:colours({ 0, 191, 0, 255}, { 0, 255, 0, 255}, { 0, 127, 0, 255}, { 63, 63, 63, 255})
@@ -31,7 +32,8 @@ function MainMenu:new(sceneManager)
     self.em:add(quitButton)
     self.em:add(menuText)
     self.em:add(self.textField)
-    self.em:add(slider)
+    self.em:add(horizontalSlider)
+    self.em:add(verticalSlider)
 
     self.click = function(btn) self:onClick(btn) end
     self.sliderChanged = function(slider) self:onSliderChanged(slider) end
--- src/utils/slider.lua
+++ src/utils/slider.lua
@@ -5,14 +5,19 @@ local Utils = require('src.utils.utils')
 local Slider = Class:derive('Slider')
 
 -- x coord is left side, y coord is centre
-function Slider:new(x, y, w, h, id)
+function Slider:new(x, y, w, h, id, isVertical)
     self.pos = Vector2(x, y)
     self.barSize = Vector2(w, h)
     self.id = id or ''
-    self.nubSize = Vector2(10, 20)
+    self.isVertical = isVertical or false
+    if self.isVertical then
+        self.nubSize = Vector2(20, 10)
+    else
+        self.nubSize = Vector2(10, 20)
+    end
     self.value = 0
     self.prevValue = self.value
-    self.xDelta = 0
+    self.delta = 0
 
     -- Slider colours
     self.grooveColour = Utils.colour(191)
@@ -34,22 +39,39 @@ function Slider:update(dt)
     if not self.enabled then return end
     local mx, my = love.mouse.getPosition()
     local leftClick = love.mouse.isDown(1)
-    if Utils.pointInRect(
-        { x = mx, y = my }, {
-            x = self.pos.x + (self.value * (self.barSize.x - self.nubSize.x)),
+    local rect
+    if self.isVertical then
+        rect = {
+            -- x = self.pos.x - self.nubSize.x / 2,
+            -- y = self.pos.y + self.value * self.barSize.y - self.barSize.y / 2,
+            x = self.pos.x - self.nubSize.x / 2, 
+            y = self.pos.y - self.barSize.y / 2 + (self.value * (self.barSize.y - self.nubSize.y)),
+            w = self.nubSize.x,
+            h = self.nubSize.y 
+        }
+    else
+        rect = {
+            -- x = self.pos.x + self.value * self.barSize.x - self.barSize.x / 2,
+            -- y = self.pos.y - self.nubSize.y / 2,
+            x = self.pos.x - self.barSize.x / 2 + (self.value * (self.barSize.x - self.nubSize.x)), 
             y = self.pos.y - self.nubSize.y / 2,
             w = self.nubSize.x, 
             h = self.nubSize.y 
         }
-    ) then
+    end
+    if Utils.pointInRect({ x = mx, y = my }, rect) then
         if leftClick then
             -- self.colour = self.pressed
             if not self.prevLeftClick then
                 self.movingSlider = true
-                self.xDelta = self.value * self.barSize.x - mx
+                if self.isVertical then
+                    self.delta = self.value * self.barSize.y - my
+                else
+                    self.delta = self.value * self.barSize.x - mx
+                end
             end
         else
-            -- self.colour = self.highlight
+            self.colour = self.highlight
         end
     else
         self.colour = self.normal
@@ -62,7 +84,11 @@ function Slider:update(dt)
     if self.movingSlider then
         self.prevValue = self.value
         self.colour = self.highlight
-        self.value = (mx + self.xDelta) / self.barSize.x
+        if self.isVertical then
+            self.value = (my + self.delta) / self.barSize.y
+        else
+            self.value = (mx + self.delta) / self.barSize.x
+        end
         if self.value > 1 then
             self.value = 1
         elseif self.value < 0 then
@@ -82,7 +108,7 @@ function Slider:draw()
     love.graphics.setColor(self.grooveColour)
     love.graphics.rectangle(
         'fill', 
-        self.pos.x, 
+        self.pos.x - self.barSize.x / 2, 
         self.pos.y - self.barSize.y / 2, 
         self.barSize.x, 
         self.barSize.y,
@@ -91,14 +117,18 @@ function Slider:draw()
     )
     love.graphics.setColor(self.colour)
     -- love.graphics.circle('fill', self.pos.x + (self.value * self.barSize.x), self.pos.y, self.nubSize.x / 2)
+    local sliderX, sliderY
+    if self.isVertical then
+        sliderX, sliderY = self.pos.x - self.nubSize.x / 2, self.pos.y - self.barSize.y / 2 + (self.value * (self.barSize.y - self.nubSize.y))
+    else
+        sliderX, sliderY = self.pos.x - self.barSize.x / 2 + (self.value * (self.barSize.x - self.nubSize.x)), self.pos.y - self.nubSize.y / 2
+    end
     love.graphics.rectangle(
         'fill', 
-        self.pos.x + (self.value * (self.barSize.x - self.nubSize.x)), 
-        self.pos.y - self.nubSize.y / 2, 
+        sliderX,
+        sliderY,
         self.nubSize.x, 
         self.nubSize.y,
-        -- self.nubSize.x / 2,
-        -- self.nubSize.y / 2
         0,
         0
     )
